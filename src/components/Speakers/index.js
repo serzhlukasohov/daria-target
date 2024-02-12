3dace70d3e60cdbe521a22b94b65bb9ae1d441bd
Serhii Lukashov <serhii_lukashov@epam.com>
2024-02-12 13:18:07 +0100

    Added Speakers section

--- src/components/Speakers/index.js
+++ src/components/Speakers/index.js
@@ -1,31 +1,33 @@
 import React from "react";
-import styled from "./Speakers.module.css";
+import Image from 'next/image'
 
-function Speaker({ name, image }) {
-    return (
-      <div className="flex flex-col items-center">
-        <div className="dotted-border p-2">
-          <img
-            className="rounded-full w-24 h-24"
-            src={`https://placehold.co/96x96?text=${image}`}
-            alt={`Blurred image of a ${name}`}
-          />
-        </div>
-        <span className="mt-2">{name}</span>
-      </div>
-    );
-  }
+import styles from "./Speakers.module.css";
+import SpeakerImage from './images/speaker.png';
+
+function Speaker({ name, imageSrc }) {
+  return (
+    <div className={styles.speaker}>
+        <Image
+          width="278"
+          height="278"
+          className={styles.speakerImg}
+          src={imageSrc}
+        />
+      <span className={styles.speakerName}>{name}</span>
+    </div>
+  );
+}
 
 const Speakers = () => {
   return (
-    <div className="flex justify-center items-center flex-col bg-gradient-to-b from-white to-gray-100 h-screen">
-      <h1 className="text-4xl font-semibold mb-10">СПИКЕРЫ — СОВСЕМ СКОРО</h1>
-      <div className="flex space-x-4">
-        <Speaker name="Дизайнер" image="designer-blurred.jpg" />
-        <Speaker name="Бухгалтер" image="accountant-blurred.jpg" />
-        <Speaker name="Психолог" image="psychologist-blurred.jpg" />
-        <Speaker name="Таргетолог" image="targetologist-blurred.jpg" />
-        <Speaker name="Айтишник" image="it-specialist-blurred.jpg" />
+    <div className={styles.speakersContainer}>
+      <h2 className="sectionTitle">СПИКЕРЫ — СОВСЕМ СКОРО</h2>
+      <div className={styles.listSpeakers}>
+        <Speaker name="Дизайнер" imageSrc={SpeakerImage} />
+        <Speaker name="Бухгалтер" imageSrc={SpeakerImage} />
+        <Speaker name="Психолог" imageSrc={SpeakerImage} />
+        <Speaker name="Таргетолог" imageSrc={SpeakerImage} />
+        <Speaker name="Айтишник" imageSrc={SpeakerImage} />
       </div>
     </div>
   );
