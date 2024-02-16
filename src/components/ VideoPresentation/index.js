83f76242405e8b20ddb0e12911a788eb0bdba7f3
KacanPasha <129742576+KacanPasha@users.noreply.github.com>
2024-02-16 09:29:42 +0200

    Video Presentation

--- src/components/ VideoPresentation/index.js
+++ src/components/ VideoPresentation/index.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import styles from './VideoPresentation.module.css';
+import React from "react";
+import Image from "next/image";
 
-const VideoPresentation = () => { 
-    return (
-       
-        <div className="min-h-screen bg-gradient-to-br from-purple-200 via-purple-100 to-purple-50 flex justify-center items-center">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold text-gray-800 mb-2">
-              ВИДЕО ПРЕЗЕНТАЦИЯ
-            </h1>
-            <div className="text-purple-600 text-2xl mb-6">
-              <i className="fas fa-heart"></i>
-            </div>
-            <p className="text-lg text-gray-700 mb-4">
-              Соприкоснись с курсом ProTarget
-            </p>
-            <button className="bg-yellow-300 text-yellow-900 font-medium py-2 px-6 rounded-full mb-10">
-              Программа курса
-            </button>
-            <div className="flex justify-center">
-              <img
-                src="https://placehold.co/100x100"
-                alt="Placeholder image with a light purple background and a gallery icon in the center"
-                className="border-2 border-purple-300 rounded-lg"
-              />
-            </div>
+import styles from "./VideoPresentation.module.css";
+import StubIcon from "./images/stub.svg";
+import HeartIcon from "./images/heart.svg";
+
+const VideoPresentation = () => {
+  return (
+    <section className={styles.videoPresentationContainer}>
+      <div>
+        <div className={styles.videoPresentationBlock}>
+          <h2 className="text-4xl font-bold text-gray-800 mb-2">
+            ВИДЕО ПРЕЗЕНТАЦИЯ
+          </h2>
+          <div className="text-purple-600 text-2xl mb-6">
+            <Image width="44" height="52" src={HeartIcon} />
           </div>
         </div>
-    )
+        <p className="text-lg text-gray-700 mb-4">
+          Соприкоснись с курсом ProTarget
+        </p>
+        <button className={`button ${styles.fillBtn}`}>Заполнить анкету</button>
+      </div>
+
+      <div className={styles.videoPresentationStub}>
+        <Image width="75" height="77" src={StubIcon} />
+      </div>
+    </section>
+  );
 };
 
 export default VideoPresentation;
