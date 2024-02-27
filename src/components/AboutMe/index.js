da55d08d2098a5c8c9a096a6cf73b31aad8d65cc
KacanPasha <129742576+KacanPasha@users.noreply.github.com>
2024-02-27 16:57:53 +0200

    section about me, mobile version

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -8,9 +8,9 @@ const AboutMe = () => {
   return (
     <section id="about">
       <div className={styles.aboutMeContainer}>
-        <h2 className="sectionTitle">ОБО МНЕ</h2>
+        <h2 className={styles.aboutMeTitle}>ОБО МНЕ</h2>
         <div className={styles.aboutMeBlock}>
-          <div>
+          <div className={styles.aboutMeImgBox}>
             <Image
               className={styles.aboutMeImg}
               width="419"
@@ -24,7 +24,7 @@ const AboutMe = () => {
               <span className={styles.aboutMeTextSpan}>
                 Привет! Меня зовут Дарья Левшина!
               </span>
-              <p>
+              <p >
                 Я живу в Польше и с 2022 года я Таргетолог, работаю с
                 предпринимателями, салонами, блогерами, онлайн-школами и
                 другими, мой доход ОТ 2000$ месяц.
@@ -33,8 +33,8 @@ const AboutMe = () => {
                 Первый свой курс по таргету я покупала работая в найме (в
                 рассрочку) всегда мечтала работать удаленно на себя несколько
                 часов в день, чтобы больше времени уделять себе, семье и
-                путешествиям. <br/> Для себя я разрушила систему жизни от ЗП до ЗП и
-                работы на пенсию. <br/> И воплотила свою мечту достаточно быстро с
+                путешествиям. <br className={styles.br} /> Для себя я разрушила систему жизни от ЗП до ЗП и
+                работы на пенсию. <br className={styles.br}/> И воплотила свою мечту достаточно быстро с
                 помощью Фриланса! Мой доход, зависит только от меня и я
                 продолжаю работать в таргете, а также помогаю всем кто этого
                 хочет.
@@ -60,6 +60,24 @@ const AboutMe = () => {
             </div>
           </div>
         </div>
+        <div className={styles.aboutMeBlockFlexTab}>
+              <div className={styles.aboutMeFlexBlock}>
+                <span className={styles.aboutMeTextSpan}>от 2000$</span>
+                <p>доход в месяц </p>
+              </div>
+              <div className={styles.aboutMeFlexBlock}>
+                <span className={styles.aboutMeTextSpan}>40+</span>
+                <p>успешных проектов</p>
+              </div>
+              <div className={styles.aboutMeFlexBlock}>
+                <span className={styles.aboutMeTextSpan}>Европа</span>
+                <p>гео</p>
+              </div>
+              <div className={styles.aboutMeFlexBlock}>
+                <span className={styles.aboutMeTextSpan}>50</span>
+                <p>выпускников</p>
+              </div>
+            </div>
       </div>
     </section>
   );
